a6a3814587e20e1dd8fa97f570a93520df8bdc2f
jnhustin <jnhustin@gmail.com>
2017-02-15 21:10:56 -0800

    minor code refactor: checkside fx's, updates their occupied value

--- js/main.js
+++ js/main.js
@@ -413,65 +413,79 @@ $('document').ready(function() {
 
 	}
 	function checkTopSide(i, j) {
+		var topObj = gameBoardArr[i][j];
+		var botObj = gameBoardArr[i - 1][j];
 		if (i === 0) {
 			return;
-		} else if (gameBoardArr[i][j].top.type === 'castle') {
-			if (gameBoardArr[i - 1][j].bottom.type === 'castle') {
-				gameBoardArr[i][j].top.paired = true;
-				gameBoardArr[i - 1][j].bottom.paired = true;
+		} else if (topObj.top.type === 'castle') {
+			if (botObj.bottom.type === 'castle') {
+				topObj.top.paired = true;
+				botObj.bottom.paired = true;
 				// if occupied, flips paired castle's occupant to match that of its pair's occupant
-				if (gameBoardArr[i][j].top.occupied) {
-					gameBoardArr[i - 1][j].bottom.occupant = gameBoardArr[i][j].top.occupant;
-				} else if (gameBoardArr[i - 1][j].bottom.occupied) {
-					gameBoardArr[i][j].top.occupant = gameBoardArr[i - 1][j].bottom.occupant;
+				if (topObj.top.occupied) {
+					botObj.bottom.occupied = topObj.top.occupied;
+					botObj.bottom.occupant = topObj.top.occupant;
+				} else if (botObj.bottom.occupied) {
+					topObj.top.occupied = botObj.bottom.occupied;
+					topObj.top.occupant = botObj.bottom.occupant;
 				}
 			}
 		}
 	}
 	function checkRightSide(i, j) {
+		var rightObj = gameBoardArr[i][j];
+		var leftObj = gameBoardArr[i][j + 1];
+
 		if (j === 3) {
 			return;
-		} else if (gameBoardArr[i][j].right.type === 'castle') {
-			if (gameBoardArr[i][j + 1].left.type === 'castle') {
-				gameBoardArr[i][j].right.paired = true;
-				gameBoardArr[i][j + 1].left.paired = true;
+		} else if (rightObj.right.type === 'castle' && leftObj.left.type === 'castle') {
+			rightObj.right.paired = true;
+			leftObj.left.paired = true;
 				
-				if (gameBoardArr[i][j].right.occupied) {
-					gameBoardArr[i][j + 1].left.occupant = gameBoardArr[i][j].right.occupant;
-				} else if (gameBoardArr[i][j + 1].left.occupied) {
-					gameBoardArr[i][j].right.occupant = gameBoardArr[i][j + 1].left.occupant;
-				}
+			if (rightObj.right.occupied) {
+				leftObj.left.occupied = rightObj.right.occupied;
+				leftObj.left.occupant = rightObj.right.occupant;
+			} else if (leftObj.left.occupied) {
+				rightObj.right.occupied = leftObj.left.occupied;
+				rightObj.right.occupant = leftObj.left.occupant;
 			}
 		}
 	}
 	function checkBottomSide(i, j) {
+		var botObj = gameBoardArr[i][j];
+		var topObj = gameBoardArr[i + 1][j];
 		if (i === 3) {
 			return;
-		} else if (gameBoardArr[i][j].bottom.type === 'castle') {
-			if (gameBoardArr[i + 1][j].top.type === 'castle') {
-				gameBoardArr[i][j].bottom.paired = true;
-				gameBoardArr[i + 1][j].top.paired = true;
+		} else if (botObj.bottom.type === 'castle') {
+			if (topObj.top.type === 'castle') {
+				botObj.bottom.paired = true;
+				topObj.top.paired = true;
 
-				if (gameBoardArr[i][j].bottom.occupied) {
-					gameBoardArr[i + 1][j].top.occupant = gameBoardArr[i][j].bottom.occupant;
-				} else if (gameBoardArr[i + 1][j].top.occupied) {
-					gameBoardArr[i][j].bottom.occupant = gameBoardArr[i + 1][j].top.occupant;
+				if (botObj.bottom.occupied) {
+					topObj.top.occupied = botObj.bottom.occupied;
+					topObj.top.occupant = botObj.bottom.occupant;
+				} else if (topObj.top.occupied) {
+					botObj.bottom.occupant = topObj.top.occupant;
 				}
 			}
 		}
 	}
 	function checkLeftSide(i, j) {
+		var leftObj = gameBoardArr[i][j];
+		var rightObj = gameBoardArr[i][j - 1];
 		if (j === 0) {
 			return;
-		} else if (gameBoardArr[i][j].left.type === 'castle') {
-			if (gameBoardArr[i][j - 1].right.type === 'castle') {
-				gameBoardArr[i][j].left.paired = true;
-				gameBoardArr[i][j - 1].right.paired = true;
+		} else if (leftObj.left.type === 'castle') {
+			if (rightObj.right.type === 'castle') {
+				leftObj.left.paired = true;
+				rightObj].right.paired = true;
 
-				if (gameBoardArr[i][j].left.occupied) {
-					gameBoardArr[i][j - 1].right.occupant = gameBoardArr[i][j].left.occupant;
-				} else if (gameBoardArr[i][j - 1].right.occupied) {
-					gameBoardArr[i][j].left.occupant = gameBoardArr[i][j - 1].right.occupant;
+				if (leftObj.left.occupied) {
+					rightObj.right.occupied = leftObj.left.occupied;
+					rightObj.right.occupant = leftObj.left.occupant;
+				} else if (rightObj.right.occupied) {
+					leftObj.left.occupied = rightObj.right.occupied;
+					leftObj.left.occupant = rightObj.right.occupant;
 				}
 			}
 		}
